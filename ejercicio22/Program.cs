1a4d0da073c878708091cbd1b227406aa69dfc45
Usuario <Usuario@DESKTOP-8F707LD>
2023-08-22 14:42:46 -0300

    Agregar archivos de proyecto.

---
+++ ejercicio22/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ejercicio22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahrenheit;
+            double celsius;
+            Console.WriteLine("conversion de temperaturas");
+            Console.Write("Ingrese el valor pemperatura");
+            var temperatura = double.Parse(Console.ReadLine());
+            Console.Write("seleccione la escala de temperatura (C)elsius o (F)ahrenheit");
+            var escala = Console.ReadLine();
+            if (escala.ToLower() == "c")
+            {
+                fahrenheit = 1.8 * temperatura + 32;
+                Console.WriteLine($"{temperatura}=celsius quivalen a{fahrenheit}grado fahrenheit");
+
+            }
+            else
+            {
+                if (escala.ToLower() == "f")
+                {
+                    celsius = (temperatura - 32) * 5 / 9;
+                    Console.WriteLine($"{temperatura} fahrenheit equivalen a {celsius} grado celsius");
+                }
+                else
+                {
+                    Console.WriteLine("escala no valida");
+                }
+            }
+        }
+    }
+}
